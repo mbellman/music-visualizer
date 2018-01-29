c908fbfcbf3e8ada7196c69ebf7cee30788cf756
mbellman <mbellman@nwe.com>
2018-01-29 06:01:40 -0600

    Added handling for a note "spread factor"

--- source/App/State/Initializers.ts
+++ source/App/State/Initializers.ts
@@ -3,15 +3,15 @@ import { IAppState, ViewMode } from '@state/Types';
 
 export const initialShapeTemplate: IShapeTemplate = {
   shapeType: ShapeTypes.BAR,
-  size: 20
+  size: 12
 };
 
 export const initialGlowTemplate: IGlowTemplate = {
   effectType: EffectTypes.GLOW,
   color: '00f',
-  blur: 5,
-  fadeIn: 0,
-  fadeOut: 0,
+  blur: 15,
+  fadeIn: 250,
+  fadeOut: 250,
   isSelected: false,
   isDelayed: false
 };
@@ -35,7 +35,7 @@ export const initialCustomizerState: ICustomizer = {
   settings: {
     framerate: 60,
     scrollSpeed: 100,
-    focusDelay: 1000,
+    focusDelay: 2000,
     tempo: 0,
     width: 1190,
     height: 640
--- source/AppCore/Visualization/ShapeFactory.ts
+++ source/AppCore/Visualization/ShapeFactory.ts
@@ -7,6 +7,8 @@ import Visualizer from '@core/Visualization/Visualizer';
 import { ShapeTypes } from '@core/Visualization/Types';
 
 export default class ShapeFactory extends CustomizerManager {
+  public static readonly SPREAD_FACTOR: number = 1.5;
+
   private get _beatsPerSecond (): number {
     return this._tempo / 60;
   }
@@ -14,7 +16,7 @@ export default class ShapeFactory extends CustomizerManager {
   private get _pixelsPerSecond (): number {
     const { framerate, scrollSpeed } = this.getCustomizerSettings();
 
-    return framerate * (60 / framerate) * Visualizer.TICK_CONSTANT * this._tempo * (scrollSpeed / 100);
+    return Visualizer.TICK_CONSTANT * 60 * this._tempo * (scrollSpeed / 100);
   }
 
   private get _tempo (): number {
@@ -50,6 +52,26 @@ export default class ShapeFactory extends CustomizerManager {
     const { height } = this.getCustomizerSettings();
     const heightRatio: number = height / Note.MAX_PITCH;
 
-    return height - (pitch * heightRatio);
+    return (
+      /**
+       * {{height}} represents the bottom edge of the rendering area (since our
+       * coordinate system sets the top edge at y = 0), {{pitch}} represents a note
+       * pitch within the range [0, MAX_PITCH], and {{heightRatio}} is a scaling factor
+       * to scale the aforementioned range to [0, height]. By subtracting the scaled
+       * pitch value from the bottom edge, higher notes will appear closer to the top
+       * of the rendering area. {{SPREAD_FACTOR}} scales the vertical note spread.
+       */
+      (height - (pitch * heightRatio)) * ShapeFactory.SPREAD_FACTOR
+      /**
+       * Having used {{SPREAD_FACTOR}} to adjust our vertical note spread, we need to
+       * shift the notes partially back into view so they still "center" on the vertical
+       * midpoint of the rendering area. {{SPREADFACTOR - 1}} gives us the percentage
+       * by which the vertical rendering area has increased (e.g. [1.5 - 1] -> 0.5),
+       * and we divide this by 2 to shift back only to a halfway point. By multiplying
+       * the result by {{height}} we determine the exact pixel amount to shift back,
+       * and subtract it from the first expression.
+       */
+      - ((ShapeFactory.SPREAD_FACTOR - 1) / 2) * height
+    );
   }
 }
